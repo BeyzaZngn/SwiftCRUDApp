5ee6d9da2eee4baecb9f64c20aebbcdaf53bd867
Beyza Zengin <112340720+BeyzaZngn@users.noreply.github.com>
2025-09-30 18:35:34 +0300

    feat(posts): add posts view controller
    Introduce a UIKit-backed `PostsViewController` as the foundation of the CRUD flow.

---
+++ SwiftCRUDApp/SwiftCRUDApp/Views/PostsViewController.swift
@@ -0,0 +1,18 @@
+//
+//  PostViewController.swift
+//  SwiftCRUDApp
+//
+//  Created by Beyza Zengin on 30.09.2025.
+//
+
+import UIKit
+
+class PostsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
