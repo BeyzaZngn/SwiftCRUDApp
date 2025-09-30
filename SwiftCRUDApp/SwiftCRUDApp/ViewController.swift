3635a44cd04802eca0ae34d87c6af9d2349f8da3
Beyza Zengin <112340720+BeyzaZngn@users.noreply.github.com>
2025-09-30 17:42:54 +0300

    Initial commit

---
+++ SwiftCRUDApp/SwiftCRUDApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SwiftCRUDApp
+//
+//  Created by Beyza Zengin on 30.09.2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
