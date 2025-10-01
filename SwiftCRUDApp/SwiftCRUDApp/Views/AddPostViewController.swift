930da8c4141670db08dd36b1a3a1196796c3cac0
Beyza Zengin <112340720+BeyzaZngn@users.noreply.github.com>
2025-10-01 17:24:09 +0300

    feat(ui): scaffold post form controllers
     Introduce placeholders for adding and editing posts to unblock future UI work.

---
+++ SwiftCRUDApp/SwiftCRUDApp/Views/AddPostViewController.swift
@@ -0,0 +1,8 @@
+//
+//  AddPostViewController.swift
+//  SwiftCRUDApp
+//
+//  Created by Beyza Zengin on 1.10.2025.
+//
+
+import Foundation
---
+++ SwiftCRUDApp/SwiftCRUDApp/Views/EditPostViewController.swift
@@ -0,0 +1,8 @@
+//
+//  EditPostViewController.swift
+//  SwiftCRUDApp
+//
+//  Created by Beyza Zengin on 1.10.2025.
+//
+
+import Foundation
